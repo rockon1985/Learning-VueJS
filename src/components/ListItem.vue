06d0905a45b633428c7430563a9bbbb9ec661330
Ketan <ksaxena@gammastack.com>
2020-05-11 17:56:57 +0530

    Basic Todo App in vuejs

---
+++ src/components/ListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <input type="checkbox" v-model="task.completed">
+    <span v-bind:class="{ done: task.completed }">{{task.text}}</span>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: ['task']
+}
+</script>
+
+<style scoped>
+
+.done {
+  text-decoration: line-through;
+  color: gray;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>{{ title }}</h1>
+      <span>{{`${tasks.filter(i => i.completed).length} / ${tasks.length} Completed`}}</span>
+      <ul>
+        <form v-on:submit.prevent="addTask" class="add-task">
+          <input type="text" name="task" placeholder="Add task">
+          <button type="submit">Add</button>
+        </form>
+        <ListItem
+          v-for="task in tasks"
+          v-bind:key = "task.id"
+          v-bind:task = "task"
+        ></ListItem>
+      </ul>
+    </div>
+    <div class="rawData">
+      {{tasks}}
+    </div>
+  </div>
+</template>
+
+<script>
+import ListItem from './ListItem.vue'
+
+export default {
+  name: 'Todo',
+  props: ['title', 'list'],
+  data: function() {
+    return {
+      tasks: [
+      { id: 1, text: 'Learn JavaScript', completed: false },
+      { id: 2, text: 'Learn Vue', completed: false },
+      { id: 3, text: 'Build something awesome', completed: false }
+    ]
+    }
+  },
+  components: {
+    ListItem
+  },
+  methods: {
+    addTask: function (event) {
+      const text = event.target.elements.task.value
+      this.tasks.push({
+        id: this.tasks.length + 1,
+        text,
+        completed: false
+      })
+      event.target.elements.task.value = ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.container ul {
+  list-style: none;
+  padding: 0;
+}
+.container li {
+  font-size: 1.4em;
+  margin: 0 10px;
+}
+
+.container {
+  padding: 5px;
+  width: 50%;
+  margin: auto;
+  background: wheat;
+  border-radius: 10px;
+  margin: 10px auto;
+  box-shadow: 2px 4px 4px lightgray;
+}
+.rawData {
+  width: 50%;
+  background: lightgray;
+  min-height: 70px;
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 0.9em;
+  margin: auto;
+  padding: 5px;
+}
+
+.add-task input {
+  margin-bottom: 15px;
+  width: 70%;
+  height: 22px;
+  padding: 0 5px;
+}
+
+.add-task button {
+  margin-bottom: 15px;
+  width: 15%;
+  height: 26px;
+  margin-left: 2px;
+  font-size: 14px;
+}
+</style>
